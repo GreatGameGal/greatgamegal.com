375cb0e8e6a07da27d4ec3368b394515f735447f
greatgamegal <thegreatgamegal@gmail.com>
2020-09-06 19:19:00 -0700

    Added HTTP redirect to HTTPS.

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ const app = express();
 
 app.use("/", express.static(__dirname + "/public_html/"));
 
+// Create HTTPS server.
 https
   .createServer(
     {
       key: fs.readFileSync(keyPath, "utf-8"),
-      cert: fs.readFileSync(certPath, "utf-8")
+      cert: fs.readFileSync(certPath, "utf-8"),
     },
     app
   )
   .listen(port, () => {
     console.log(`App is now listening on ${port}`);
   });
+
+// Creates HTTP server to redirect to HTTPS server.
+const http = express();
+http.get("*", (req, res) => {
+  res.redirect("https://" + req.headers.host + req.url);
+});
+http.listen(80);
