345d50b8bc56c225b93ca5134fba299340db72ed
greatgamegal <thegreatgamegal@gmail.com>
2020-09-06 19:12:52 -0700

    Completely refactored to make HTTPS work.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require("http");
 const https = require("https");
 
 const fs = require("fs");
@@ -6,20 +5,16 @@ const express = require("express");
 const { port, keyPath, certPath, chainPath } = require("./config/config.js");
 const app = express();
 
-const server =
-  fs.existsSync(keyPath) && fs.existsSync(certPath) && fs.existsSync(chainPath)
-    ? https.createServer(
-        {
-          "key": fs.readFileSync(keyPath, "utf-8"),
-          "cert": fs.readFileSync(certPath, "utf-8"),
-          "ca": fs.readFileSync(chainPath, "utf-8")
-        },
-        app
-      )
-    : http.createServer(app);
+app.use("/", express.static(__dirname + "/public_html/"));
 
-app.use("/", express.static("./public_html/"));
-
-server.listen(port, () => {
-  console.log(`App is now listening on ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync(keyPath, "utf-8"),
+      cert: fs.readFileSync(certPath, "utf-8")
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`App is now listening on ${port}`);
+  });
