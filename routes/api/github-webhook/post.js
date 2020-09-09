106f1e17d0a01a9059cac031005a5f5565d9de46
greatgamegal <thegreatgamegal@gmail.com>
2020-09-09 15:18:35 -0700

    Added test logging.

--- routes/api/github-webhook/post.js
+++ routes/api/github-webhook/post.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
 module.exports = function (req, res, next) {
+  console.log("Got here");
   req.on("data", (chunk) => {
     if (
       req.headers["x-hub-signature"] ==
