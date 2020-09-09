3a3bd0878c5353b08f84f91e49e668f9505e1f29
greatgamegal <thegreatgamegal@gmail.com>
2020-09-09 15:36:19 -0700

    Push to check variables.

--- routes/api/github-webhook/post.js
+++ routes/api/github-webhook/post.js
@@ -1,9 +1,10 @@
 const crypto = require("crypto");
 
 module.exports = function (req, res, next) {
-  console.log(this.config);
   const hmac = crypto.createHmac("sha1", this.config.github_secret);
   const sig = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+  console.log(sig);
+  console.log(req.headers["x-hub-signature"]);
   if (
     crypto.timingSafeEqual(
       Buffer.from(req.headers["x-hub-signature"]),
