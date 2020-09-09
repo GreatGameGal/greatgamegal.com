df3a66672b1d0a7e467cf59a60682f05f2eff4b2
greatgamegal <thegreatgamegal@gmail.com>
2020-09-09 15:30:17 -0700

    Maybe fixed github webhook stuff.

--- routes/api/github-webhook/post.js
+++ routes/api/github-webhook/post.js
@@ -2,22 +2,20 @@ const crypto = require("crypto");
 
 module.exports = function (req, res, next) {
   console.log(req.headers["x-hub-signature"]);
-  req.on("data", (chunk) => {
-    console.log(
-      "sha1=" +
-        crypto
-          .createHmac("sha1", this.config.github_secret)
-          .update(chunk.toString())
-          .digest("hex")
-    );
-    if (
-      req.headers["x-hub-signature"] ==
-      "sha1=" +
-        crypto
-          .createHmac("sha1", this.config.github_secret)
-          .update(chunk.toString())
-          .digest("hex")
-    )
-      this.eventHandler.emit("repoupdate", {});
-  });
+  console.log(
+    "sha1=" +
+      crypto
+        .createHmac("sha1", this.config.github_secret)
+        .update(JSON.stringify(req.body))
+        .digest("hex")
+  );
+  if (
+    req.headers["x-hub-signature"] ==
+    "sha1=" +
+      crypto
+        .createHmac("sha1", this.config.github_secret)
+        .update(JSON.stringify(req.body))
+        .digest("hex")
+  )
+    this.eventHandler.emit("repoupdate", {});
 };
