6d5e0c0480742927b845815339c5651e1609a45d
greatgamegal <thegreatgamegal@gmail.com>
2020-09-09 15:40:01 -0700

    Removed error logging I forgot to remove in last commit.

--- routes/api/github-webhook/post.js
+++ routes/api/github-webhook/post.js
@@ -3,8 +3,6 @@ const crypto = require("crypto");
 module.exports = function (req, res, next) {
   const hmac = crypto.createHmac("sha1", this.config.github_secret);
   const sig = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
-  console.log(sig);
-  console.log(req.headers["x-hub-signature"]);
   if (
     crypto.timingSafeEqual(
       Buffer.from(req.headers["x-hub-signature"]),
