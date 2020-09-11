780c2adca802726d420912a1373fd598c74b9fa1
greatgamegal <thegreatgamegal@gmail.com>
2020-09-10 23:29:41 -0700

    Further updated handling to maybe work this time.

--- routes/api/github-webhook/post.js
+++ routes/api/github-webhook/post.js
@@ -1,27 +1,32 @@
 const crypto = require("crypto");
 
 module.exports = function (req, res, next) {
-  if (!res.headers || !res.headers["x-hub-signature"] || !res.body) {
-    res.sendStatus(401);
-    return;
-  }
-  const hmac = crypto.createHmac("sha1", this.config.github_secret);
-  const sig = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
-  if (
-    crypto.timingSafeEqual(
-      Buffer.from(req.headers["x-hub-signature"]),
-      Buffer.from(sig)
-    )
-  ) {
-    switch (req.headers["x-github-event"]) {
-      case "push":
-        res.sendStatus(200);
-        this.eventHandler.emit("repo" + req.headers["x-github-event"], {req: req, repo: req.body.repository});
-        break;
+  if (res.headers && res.body && res.headers["x-hub-signature"]) {
+    const hmac = crypto.createHmac("sha1", this.config.github_secret);
+    const sig = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+    if (
+      crypto.timingSafeEqual(
+        Buffer.from(req.headers["x-hub-signature"]),
+        Buffer.from(sig)
+      )
+    ) {
+      switch (req.headers["x-github-event"]) {
+        case "push":
+          res.sendStatus(200);
+          this.eventHandler.emit("repo" + req.headers["x-github-event"], {
+            req: req,
+            repo: req.body.repository,
+          });
+          break;
 
         default:
-          console.warn("Unhandled github event: " + req.headers["x-github-event"]);
+          console.warn(
+            "Unhandled github event: " + req.headers["x-github-event"]
+          );
           break;
+      }
+    } else {
+      res.sendStatus(401);
     }
   } else {
     res.sendStatus(401);
